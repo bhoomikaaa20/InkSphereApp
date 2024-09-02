f0fd9739db8e60f7114e01cb08e524d0a0d17491
Bhoomika <bhoomikasomavarapu15@gmail.com>
2024-09-02 21:24:51 +0530

    updated commit

--- server/index.js
+++ server/index.js
@@ -14,16 +14,17 @@ app.use(express.json());
 app.use(expressFileUpload());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(express.urlencoded({ extended: true }));
+
+// Updated CORS configuration
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://66d5de135cc7520d954a266f--monumental-dragon-9e376c.netlify.app", // Add your Netlify domain here
+  "https://monumental-dragon-9e376c.netlify.app",
 ];
 
 app.use(
   cors({
     credentials: true,
     origin: function (origin, callback) {
-      // Allow requests with no origin, like mobile apps or curl requests
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
         const msg = `The CORS policy for this site does not allow access from the specified origin: ${origin}`;
@@ -33,13 +34,14 @@ app.use(
     },
   })
 );
-//Listening to server
+
+// Listening to server
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`App is listening at ${PORT}`);
 });
 
-//Connection to Mongodb
+// Connection to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then((res) => console.log("Connected to db"))
